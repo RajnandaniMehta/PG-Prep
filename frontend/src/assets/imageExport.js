b159ad8263bde3b382b3d551c4823054406c7bd1
Rajnandani Mehta <rajnandanimehta635@gmail.com>
2025-08-18 00:12:38 +0530

    Images added

---
+++ frontend/src/assets/imageExport.js
@@ -0,0 +1,18 @@
+import bgImage from './bg.png';
+import logo from './logo.jpg';
+import loginbg from './loginbg.png'
+import saveLife from './saveLife.png'
+import progress from './progress.png'
+import goal from './goal.png'
+import bank from './bank.png'
+import topic from './topic.png'
+export {
+    bgImage,
+    logo,
+    loginbg,
+    saveLife,
+    progress,
+    goal,
+    bank,
+    topic
+}
